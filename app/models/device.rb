7634e24b096e8100d3506da6d5514d199308e44a
Saim Zahid <saimzahid@me.com>
2016-08-25 13:30:20 -0400

    Added "Annotate" gem

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,19 @@
+# == Schema Information
+#
+# Table name: devices
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  id_number  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#  group_name :string
+#  group_id   :integer
+#  assigned   :boolean
+#
+
 class Device < ApplicationRecord
 	belongs_to :user
 	belongs_to :group
-
 end
--- test/models/device_test.rb
+++ test/models/device_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: devices
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  id_number  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#  group_name :string
+#  group_id   :integer
+#  assigned   :boolean
+#
+
 require 'test_helper'
 
 class DeviceTest < ActiveSupport::TestCase
