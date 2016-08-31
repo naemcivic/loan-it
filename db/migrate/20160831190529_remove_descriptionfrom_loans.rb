17a7a8d9f6fd99bb486d865113d67b1876313b9b
Saim Zahid <saimzahid@me.com>
2016-08-31 16:09:20 -0400

    bring back migrations

--- db/migrate/20160831190529_remove_descriptionfrom_loans.rb
+++ db/migrate/20160831190529_remove_descriptionfrom_loans.rb
@@ -1,5 +1,5 @@
 class RemoveDescriptionfromLoans < ActiveRecord::Migration[5.0]
   def change
-  	# remove_column :loans, :description, :text
+  	remove_column :loans, :description, :text
   end
 end
