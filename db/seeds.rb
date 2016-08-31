a93ca8b565197bd49ec25e9c116b823023af13c3
Saim Zahid <saimzahid@me.com>
2016-08-31 16:00:14 -0400

    Took out user reference to device.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Device.delete_all
 
-user= User.new
-user.email = 'test@example.com'
-user.password = 'password'
-user.password_confirmation = 'password'
-user.save!
 
-@device1 = Device.create(name:'iPad #1', id_number: '1')
+Device.create(name:'iPad #1', id_number: '1')
 
-@device2 = Device.create(name:'iPad #2', id_number: '2')
+Device.create(name:'iPad #2', id_number: '2')
 
-#@device3 = Device.create(name:'iPad #3', id_number: '3')
+Device.create(name:'iPad #3', id_number: '3')
 
-@user.devices << @device1
