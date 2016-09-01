6ad7bb0ec13c139458bfe5353edc2433a26d1bf9
Saim Zahid <saimzahid@me.com>
2016-09-01 15:29:20 -0400

    Devices need to be grouped by Group.

--- app/models/device.rb
+++ app/models/device.rb
@@ -15,6 +15,7 @@
 
 class Device < ApplicationRecord
 	belongs_to :group
+
 	has_many :loans, ->() { order(created_at: :desc)}
 
 	has_one :active_loan, ->() { where(active: true) }, class_name: Loan
