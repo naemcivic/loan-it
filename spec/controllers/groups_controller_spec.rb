0df426c65d27059e2e87e7d85f7ba49307ca9e88
Saim Zahid <saimzahid@me.com>
2016-09-01 12:58:12 -0400

    Finished all tests for Group's Controller.

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe GroupsController, type: :controller do
     sign_in create(:user) # Using factory girl as an example
   end
 
-describe "GET #index" do
+  describe "GET #index" do
     before do
       group = build_stubbed(:group)
 
@@ -19,4 +19,110 @@ describe "GET #index" do
     it { should render_template(:index) }
   end
 
+  describe "POST #create" do
+    context 'when group is valid' do
+      before do
+        group = build_stubbed(:group)
+
+        allow(Group).to receive(:find).with(group.id.to_s) { group }
+
+        allow_any_instance_of(Group).to receive(:save) { true }
+
+        post :create, params: { group: attributes_for(:group) }
+      end
+
+      it { should redirect_to(@group)}
+    end
+
+    context 'when group is invalid' do
+      before do
+        group = build_stubbed(:group)
+
+        allow(Group).to receive(:find).with(group.id.to_s) { group }
+
+        allow_any_instance_of(Group).to receive(:save) { false }
+
+        post :create, params: { group: attributes_for(:group) }
+      end
+
+      it { should render_template(:new) }
+    end
+  end
+
+  describe 'GET #new' do
+    before do
+      group = build_stubbed(:group)
+
+      allow(Group).to receive(:find).with(group.id.to_s) { group }
+
+      get :new
+    end
+
+    it { should render_template(:new) }
+
+  end
+
+  describe 'DELETE #destoy' do
+
+    before do
+
+      group = build_stubbed(:group)
+
+      allow(Group).to receive(:find).with(group.id.to_s) { group }
+
+      allow(group).to receive(:destroy) { }
+
+
+      delete :destroy, params: { id: group.id }
+
+
+    end
+
+    it { should redirect_to(groups_url) }
+  end
+
+  describe 'Patch #update' do
+    context 'when group update is valid' do
+      before do
+        group = build_stubbed(:group)
+
+        allow(Group).to receive(:find).with(group.id.to_s) { group }
+
+        allow_any_instance_of(Group).to receive(:update) { true }
+
+        patch :update, params: { id: group.id, group: attributes_for(:group) }
+      end
+
+      it { should redirect_to(@group)}
+
+    end
+
+
+    context 'when group update is invalid' do
+      before do
+        group = build_stubbed(:group)
+
+        allow(Group).to receive(:find).with(group.id.to_s) { group }
+
+        allow_any_instance_of(Group).to receive(:update) { false }
+
+        patch :update, params: { id: group.id, group: attributes_for(:group) }
+      end
+
+      it { should render_template(:edit)}
+
+    end
+  end
+
+  describe "GET #edit" do
+    before do
+      group = build_stubbed(:group)
+
+      allow(Group).to receive(:find).with(group.id.to_s) { group }
+
+      get :edit, params: { id: group.id }
+    end
+
+    it { should render_template(:edit) }
+  end
 end
