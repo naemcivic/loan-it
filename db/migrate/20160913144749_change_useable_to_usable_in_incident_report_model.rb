0ef51e12b8bf15fd3dd870bd301c3cc11bbfa234
Saim Zahid <saimzahid@me.com>
2016-09-14 16:36:31 -0400

    Another spelling error.

--- db/migrate/20160913144749_change_useable_to_usable_in_incident_report_model.rb
+++ db/migrate/20160913144749_change_useable_to_usable_in_incident_report_model.rb
@@ -1,5 +1,5 @@
 class ChangeUseableToUsableInIncidentReportModel < ActiveRecord::Migration[5.0]
   def change
-  	rename_column :incident_reports, :usable, :usable
+  	rename_column :incident_reports, :useable, :usable
   end
 end
