a52d4ea8ca9fe25350f7b51e28583b10bddc0f8b
naemcivic <naemcivic@gmail.com>
2016-09-26 11:33:59 -0400

    add logincomponent in routing

--- loan-it-frontend/src/app/app.routing.ts
+++ loan-it-frontend/src/app/app.routing.ts
@@ -1,17 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import { DeviceComponent } from './devices/device.component'
 import { BrokenDeviceComponent } from './devices/broken_device.component'
-
-
+import { LoginComponent } from './login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/devices', pathMatch: 'full' },
-  {
-  	path: 'devices',
-  	component: DeviceComponent
-  },
-  { path: 'brokendevices', component: BrokenDeviceComponent }
-  // { path: 'about', component: AboutComponent}
+  { path: 'devices', component: DeviceComponent },
+  { path: 'brokendevices', component: BrokenDeviceComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 export const routing = RouterModule.forRoot(routes);
