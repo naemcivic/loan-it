3fcdc8d06e541fe1df8af590a43a0850650688eb
naemcivic <naemcivic@gmail.com>
2016-09-27 22:40:45 -0400

    modify login logic

--- loan-it-frontend/src/app/logged-in.guard.ts
+++ loan-it-frontend/src/app/logged-in.guard.ts
@@ -2,11 +2,20 @@ import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { UserService } from './shared/user.service';
 
+import { Router } from '@angular/router';
+
+
 @Injectable()
 export class LoggedInGuard implements CanActivate {
-    constructor(private user: UserService) {}
+    constructor(private user: UserService, private router: Router) {}
 
     canActivate() {
-        return this.user.isLoggedIn();
+          if (this.user.isLoggedIn()) {
+            return true;
+        } else
+        {
+            this.router.navigate(['/login'])
+            return false;
+        }
     }
 }
